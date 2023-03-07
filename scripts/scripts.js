6fbd6ae769a3cf99456ab37256526cfe378fd628
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 06:17:08 -0500

    First attempt at playRound function fails
    The switch conditional in this format actually shows up 3 different prompts instead of just one for the situation.
    I will figure out a new way to do this.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,33 +20,52 @@ function userInputFormatter() {
     return firstCharacter + restOfCharacters + finalCharacter;
 }
 
+function playRound() {
+    switch (userInput) {
+        case "Rock":
+            if (computerInput="Scissors") {
+                alert(`Computer chose ${computerInput}. You win!`)
+            } else if (computerInput="Paper") {
+                alert(`Computer chose ${computerInput}. You lose!`)
+            } else {
+                alert (`Computer also chose ${computerInput}. It's a tie!`)
+            }
+        case "Paper":
+            if (computerInput="Rock") {
+                alert(`Computer chose ${computerInput}. You win!`)
+            } else if (computerInput="Scissors") {
+                alert (`Computer chose ${computerInput}. You lose!`)
+            } else {
+                alert (`Computer also chose ${computerInput}. It's a tie!`)
+            }
+        case "Scissors":
+            if (computerInput="Paper") {
+                alert(`Computer chose ${computerInput}. You win!`)
+            } else if (computerInput="Rock") {
+                alert (`Computer chose ${computerInput}. You lose!`)
+            } else {
+                alert (`Computer also chose ${computerInput}. It's a tie!`)
+            }
+    }
+}
+
 alert("Let's play Rock, Paper, Scissors!");
+
 let userInput = prompt("Input Rock, Paper, or Scissors below.");
 userInput = userInputFormatter();
-console.log(userInput);
 
-/* 
-(function that formats the userInput so the first character is capital and the rest is lower case)
+let computerInput = getComputerChoice();
 
-userInputFormatter is a function
-
-firstCharacter is the first character of userInput
-
-restOfCharacters is the rest of the characters of userInput
-
-Capitalize firstCharacter
-
-Lower case restOfCharacters
-
-Return firstCharacter plus restOfCharacters
-
-
-*/
+playRound();
 
 /* 
 (function that compares computerInput and userInput and determines who wins the game)
-playRound is a function
 
+playRound is a function
+ 
+There are two ways of doing this I can think of: if...else statements and switch cases. If else statements would take 6 lines of code for the options that contradict (win or lose) and the default can be the 3 matches (ties)
+
+Switch cases would take up to 9 different cases, but are easier to read. 
 */
 
 
