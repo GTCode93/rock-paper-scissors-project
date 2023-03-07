d9e607e5c45ba961f9f9b13bfbaa2d3208bd2f4d
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 08:01:22 -0500

    Remove function playRound attempts
    Let's try again another time. I think what could help me a bunch here is
    
    1: sticking with the problem solving process rather than going into the code immediately
    
    2: PSEUDOCODE! Look at concepts you already know from your notes and see what you can do.
    
    3: don't overwork yourself. A tired mind isn't ideal for code, takes breaks! They're as essential as work.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,35 +20,6 @@ function userInputFormatter() {
     return firstCharacter + restOfCharacters + finalCharacter;
 }
 
-function playRound() {
-    switch (userInput) {
-        case "Rock":
-            if (computerInput="Scissors") {
-                alert(`Computer chose ${computerInput}. You win!`)
-            } else if (computerInput="Paper") {
-                alert(`Computer chose ${computerInput}. You lose!`)
-            } else {
-                alert (`Computer also chose ${computerInput}. It's a tie!`)
-            }
-        case "Paper":
-            if (computerInput="Rock") {
-                alert(`Computer chose ${computerInput}. You win!`)
-            } else if (computerInput="Scissors") {
-                alert (`Computer chose ${computerInput}. You lose!`)
-            } else {
-                alert (`Computer also chose ${computerInput}. It's a tie!`)
-            }
-        case "Scissors":
-            if (computerInput="Paper") {
-                alert(`Computer chose ${computerInput}. You win!`)
-            } else if (computerInput="Rock") {
-                alert (`Computer chose ${computerInput}. You lose!`)
-            } else {
-                alert (`Computer also chose ${computerInput}. It's a tie!`)
-            }
-    }
-}
-
 alert("Let's play Rock, Paper, Scissors!");
 
 let userInput = prompt("Input Rock, Paper, or Scissors below.");
@@ -56,16 +27,20 @@ userInput = userInputFormatter();
 
 let computerInput = getComputerChoice();
 
+let playerSelection = userInput;
+
+let computerSelection = computerInput;
+
 playRound();
 
 /* 
 (function that compares computerInput and userInput and determines who wins the game)
 
 playRound is a function
- 
-There are two ways of doing this I can think of: if...else statements and switch cases. If else statements would take 6 lines of code for the options that contradict (win or lose) and the default can be the 3 matches (ties)
 
-Switch cases would take up to 9 different cases, but are easier to read. 
+Problem: how do we figure out an algorithm to solve this? We need to write an algorithm that will compare the different variations of a rock paper scissors game. The function playRound must be able to take on two values: "playerSelection" and "computerSelection" (which i could redeclare these as the same values as userInput and computerInput)
+
+
 */
 
 
