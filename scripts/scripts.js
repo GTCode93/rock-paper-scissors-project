fdf06b21bf3b18fda2b3a7878045c420dee6acda
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 05:38:14 -0500

    Finish userInputFormatter function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,12 +8,39 @@ function getComputerChoice() {
         return "Scissors"
     }
 }
-alert("Let's play Rock, Paper, Scissors!")
+
+function userInputFormatter() {
+    firstCharacter = userInput.charAt(0);
+    restOfCharacters = userInput.slice(1, -1);
+    finalCharacter = userInput.slice(-1);
+
+    firstCharacter = firstCharacter.toUpperCase();
+    restOfCharacters = restOfCharacters.toLowerCase();
+    finalCharacter = finalCharacter.toLowerCase();
+    return firstCharacter + restOfCharacters + finalCharacter;
+}
+
+alert("Let's play Rock, Paper, Scissors!");
 let userInput = prompt("Input Rock, Paper, or Scissors below.");
+userInput = userInputFormatter();
+console.log(userInput);
 
 /* 
 (function that formats the userInput so the first character is capital and the rest is lower case)
+
 userInputFormatter is a function
+
+firstCharacter is the first character of userInput
+
+restOfCharacters is the rest of the characters of userInput
+
+Capitalize firstCharacter
+
+Lower case restOfCharacters
+
+Return firstCharacter plus restOfCharacters
+
+
 */
 
 /* 
