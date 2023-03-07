2c9f6ae80e3b1b1f3a173550f8f36522b70fd623
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 05:15:59 -0500

    Remove playerSelection function and fix userInput

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,25 @@
-function playerSelection() {
-    prompt('Input Rock, Paper, or Scissors below.');
-    
-}
-
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 100);
     if (number <= 33) {
-        return 'Rock'
+        return "Rock"
     } else if (number <= 66) {
-        return 'Paper'
+        return "Paper"
     } else if (number <= 99) {
-        return 'Scissors'
+        return "Scissors"
     }
 }
+alert("Let's play Rock, Paper, Scissors!")
+let userInput = prompt("Input Rock, Paper, or Scissors below.");
+
+/* 
+(function that formats the userInput so the first character is capital and the rest is lower case)
+userInputFormatter is a function
+*/
+
+/* 
+(function that compares computerInput and userInput and determines who wins the game)
+playRound is a function
+
+*/
+
+
