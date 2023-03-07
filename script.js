5972b6201712a95be58d6cf466af3e6a4e2f0f44
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 01:42:36 -0500

    Write pseudocode for function getComputerChoice

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+Problem: Write a function named getComputerChoice that will randomly choose between three options: 'Rock', 'Paper', or 'Scissors'
+
+No user interface
+Desired output is Rock Paper or Scissors
+No user input, the code must randomly give either of the three output randomly
+
+Pseudocode:
+getComputerChoice is a function with no parameters
+let number be a variable
+number takes on a random integer with Math.random and Math.floor (USING * 100, THE OUTCOME WILL NEVER BE 100, JUST 99 AND BELOW)
+
+if number is <= 33, then return computerChoice is "Rock"
+else if number is <= 66, then return computerChoice is "Paper"
+else number is <= 99, then return computerChoice is "Scissors"
+
+*/
+
