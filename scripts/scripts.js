b3ae5de6b14ee003bfb0e072a0dbfc9c63706e80
Gaven Tran <gaven.q.tran@gmail.com>
2023-03-07 03:16:36 -0500

    Create playerSelection function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,8 @@
-/* 
-Problem: Write a function named getComputerChoice that will randomly choose between three options: 'Rock', 'Paper', or 'Scissors'
+function playerSelection() {
+    let selection = prompt('Input Rock, Paper, or Scissors below.');
+}
 
-No user interface
-Desired output is Rock Paper or Scissors
-No user input, the code must randomly give either of the three output randomly
-
-Pseudocode:
-getComputerChoice is a function with parameter computerChoice
-let number be a variable
-number takes on a random integer with Math.random and Math.floor (USING * 100, THE OUTCOME WILL NEVER BE 100, JUST 99 AND BELOW)
-
-if number is <= 33, then return computerChoice is "Rock"
-else if number is <= 66, then return computerChoice is "Paper"
-else number is <= 99, then return computerChoice is "Scissors"
-
-*/
-
-function getComputerChoice () {
+function getComputerChoice() {
     let number = Math.floor(Math.random() * 100);
     if (number <= 33) {
         return 'Rock'
@@ -26,3 +12,7 @@ function getComputerChoice () {
         return 'Scissors'
     }
 }
+
+let playerSelection = playerSelection();
+let computerChoice = getComputerChoice();
+function playRound()
